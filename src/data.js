de1751558902039986655c27b382f3122f23a30e
nguyenminhtuk52 <79831942+nguyenminhtuk52@users.noreply.github.com>
2022-01-20 21:17:16 +0700

    Update data.js

--- src/data.js
+++ src/data.js
@@ -3,18 +3,18 @@ export default [
         id: 1,
         title: "Vinh danh 100 sinh viên FPT Polytechnic năm 2021",
         desc: "Cao đẳng FPT Polytechnic cơ sở Hà Nội năm 2021, Tòa nhà FPT Polytechnic, P. Trịnh Văn Bô, Xuân Phương, Nam Từ Liêm, Hà Nội",
-        img: "../img/anh.jpg",
+        img: "http://placeimg.com/640/480/fashion",
     },
     {
         id: 2,
         title: "Vinh danh 100 sinh viên FPT Polytechnic năm 2021",
         desc: "Cao đẳng FPT Polytechnic cơ sở Hà Nội năm 2021, Tòa nhà FPT Polytechnic, P. Trịnh Văn Bô, Xuân Phương, Nam Từ Liêm, Hà Nội",
-        img: "../img/anh.jpg",
+        img: "http://placeimg.com/640/480/fashion",
     },
     {
         id: 3,
         title: "Vinh danh 100 sinh viên FPT Polytechnic năm 2021",
         desc: "Cao đẳng FPT Polytechnic cơ sở Hà Nội năm 2021, Tòa nhà FPT Polytechnic, P. Trịnh Văn Bô, Xuân Phương, Nam Từ Liêm, Hà Nội",
-        img: "../img/anh.jpg",
+        img: "http://placeimg.com/640/480/fashion",
     },
 ];
